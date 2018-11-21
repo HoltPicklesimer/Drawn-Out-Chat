8b2a33eb56660a71eb9961861339434804b04586
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-11-20 22:04:09 -0700

    Data Layer

--- server.js
+++ server.js
@@ -50,8 +50,23 @@ function getChatRoom(req, res) {
 		if (error || result == null || result.length != 1) {
 			res.status(500).json({success:false, data:error});
 		} else {
-			var user = result[0];
+			var room = result[0];
 			res.status(200).json(result[0]);
+		}
+	});
+}
+
+// Get the chat rooms of a user
+function getUserRooms(req, res) {
+	console.log("Getting User in Chat relationship " + req.query.id);
+	var id = req.query.id;
+	console.log("Trying to connect to a database at " + connectionString);
+	getUserRoomsFromDb(id, function (error, result) {
+		if (error || result == null) {
+			res.status(500).json({success:false, data:error});
+		} else {
+			var rooms = result;
+			res.status(200).json(result);
 		}
 	});
 }
@@ -60,8 +75,8 @@ var queries = [
 	"SELECT id, username, password FROM users WHERE id = $1::int",
 	"SELECT id, username, password FROM users WHERE username = $1::varchar AND password = $2::varchar",
 	"SELECT id, name, admin_id, image_data FROM chat_rooms WHERE id = $1::int",
-	"SELECT id, user_id, chat_id FROM chat_users WHERE id = $1::int",
-	"SELECT id, user_id, chat_id FROM chat_users WHERE user_id = $1::int AND chat_id = $2::int",
+	"SELECT id, user_id, chat_id FROM chat_users WHERE user_id = $1::int",
+	"SELECT id, user_id, chat_id FROM chat_users WHERE chat_id = $1::int",
 	"SELECT id, chat_id, user_id, content, date_published FROM comments WHERE id = $1::int",
 	"SELECT id, chat_id, user_id, content, date_published FROM comments WHERE chat_id = $1 ORDER BY date_published DESC"
 ];
@@ -100,6 +115,27 @@ function getRoomFromDb(id, callback) {
 	var params = [id];
 
 	console.log("Getting chat room now, id = " + id);
+
+	pool.query(sql, params, function(err, result){
+
+		if (err) {
+			console.log("Error in query: ");
+			console.log(err);
+			callback(err, null);
+		}
+
+		console.log("Found result: " + JSON.stringify(result.rows));
+
+		callback(null, result.rows);
+	});
+}
+
+// Get chat rooms of a user from the database
+function getUserRoomsFromDb(id, callback) {
+	var sql = queries[3];
+	var params = [id];
+
+	console.log("Getting chat rooms now, user id = " + id);
 
 	pool.query(sql, params, function(err, result){
 
