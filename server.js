2605045671d222e292331eb50c35b87af49a62f7
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-11-21 16:58:42 -0700

    Data Layer - inserts

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const path = require("path");
+const fs = require("fs");
 const selects = require("./selects");
 const inserts = require("./inserts");
 
@@ -18,7 +19,7 @@ app.listen(app.get('port'), function(req, res){
 });
 
 app.get("/", function(req, res) {
-	res.sendFile()
+	res.sendFile("main.html");
 });
 app.get("/getUser", selects.getUser);
 app.get("/getChatRoom", selects.getChatRoom);
