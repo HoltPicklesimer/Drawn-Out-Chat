a83c2e006c93267ae7ce742824138551356f3110
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-11-20 21:10:20 -0700

    Created server file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const path = require("path");
 
 // Get the database url
 const connectionString = process.env.DATABASE_URL;
