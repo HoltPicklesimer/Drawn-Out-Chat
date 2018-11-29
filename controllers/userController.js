1ee947d17f7a3fc294fd70b5a223d186023f69ff
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-11-28 20:08:44 -0700

    Edit models

--- controllers/userController.js
+++ controllers/userController.js
@@ -53,7 +53,7 @@ function postUser(req, res) {
 		if (error) {
 			res.status(500).json({success:false, data:error});
 		} else {
-			result = { username:username, password:password };
+			result = { success:true, entity: {username:username, password:password } };
 			res.status(200).json(result);
 		}
 	});
