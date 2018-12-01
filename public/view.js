12fcac91e8a370861e2841c71d11722cdaa4279e
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-12-01 13:24:47 -0700

    Edit view

--- public/view.js
+++ public/view.js
@@ -121,7 +121,7 @@ function updateImage() {
 
 /* Search Users */
 function searchUsers() {
-	var item = $("#userSearch").value;
+	var item = $("#userSearch").text;
 	console.log(item);
 	var params = { item:item };
 	$.get(url + "searchUsers", params, function(data, status){
