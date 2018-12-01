feb3a2121ae634c325c100e612372522e63d5e38
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-11-30 23:10:08 -0700

    Edit view

--- public/view.js
+++ public/view.js
@@ -1,7 +1,7 @@
 var user_id = 13;
 var chat_id = 4;
 
-$(function(){ setInterval(oneSecondFunction, 1000);}); // set a clock to update the image every second
+$(function(){ setInterval(update, 1000);}); // set a clock to update every second
 
 var url = "https://gentle-tundra-31449.herokuapp.com/";
 
@@ -80,8 +80,8 @@ function loadImage(data) {
   redraw();
 }
 
-function oneSecondFunction() {
-	// stuff you want to do every second
+function update() {
+	loadImage();
 }
 
 // function search() {
--- server.js
+++ server.js
@@ -3,12 +3,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 const path = require("path");
 const fs = require("fs");
-const http = require("http").Server(app);
-var io = require('socket.io')(http);
-
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
 
 const userController = require("./controllers/userController");
 const chatController = require("./controllers/chatController");
