7e22d9ec387cb4b9395d298334a7317ae2f130a1
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-12-05 16:34:40 -0700

    Delete Rooms

--- public/view.js
+++ public/view.js
@@ -49,7 +49,7 @@ function loadUserRooms() {
 			$("#selectRoom").empty();
 			for (var i = 0; i < data.length; ++i)
 				$("#selectRoom").append("<option value='" + data[i].id + "'>" + data[i].name + "</option>");
-			if (typeof chat_id == 'undefined' || document.getElementById("selectRoom").value == '')
+			if (typeof chat_id == 'undefined' || document.getElementById("selectRoom").options[roomIndex].value == '')
 				loadRoom();
 			else
 				document.getElementById("selectRoom").selectedIndex = roomIndex;
@@ -59,6 +59,8 @@ function loadUserRooms() {
 
 /* When the room is requested, load the name, image, comment, and users. */
 function loadRoom() {
+	if (document.getElementById("selectRoom").options[roomIndex].value == '')
+				document.getElementById("selectRoom").selectedIndex = "0";
 	roomIndex = document.getElementById("selectRoom").selectedIndex;
 	chat_id = document.getElementById("selectRoom").options[roomIndex].value;
 	// Set up the parameters to send to the Controller
