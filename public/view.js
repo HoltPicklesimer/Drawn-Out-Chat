6f9c787359d0b4c7e52aad0acd23251c44ce23ea
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-12-05 16:51:18 -0700

    Delete Rooms

--- public/view.js
+++ public/view.js
@@ -49,7 +49,7 @@ function loadUserRooms() {
 			$("#selectRoom").empty();
 			for (var i = 0; i < data.length; ++i)
 				$("#selectRoom").append("<option value='" + data[i].id + "'>" + data[i].name + "</option>");
-			if (typeof chat_id == 'undefined' || document.getElementById("selectRoom").options[roomIndex] == '')
+			if (typeof chat_id == 'undefined' || document.getElementById("selectRoom").value == '')
 				loadRoom();
 			else
 				document.getElementById("selectRoom").selectedIndex = roomIndex;
