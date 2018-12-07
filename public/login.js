cf071647552fc9b1ed1d7b207e43c50d62c7f2f1
Holt Picklesimer <holtpicklesimer@gmail.com>
2018-12-06 22:53:33 -0700

    Added Sessions

--- public/login.js
+++ public/login.js
@@ -15,8 +15,7 @@ function login() {
 	var params = { username:user, password:pass };
 	$.post(url + "login", params, function(data, status){
 		console.log(status);
-		console.log(data);
-		if (data != 'undefined')
+		if (status == "success")
 			window.location.href = url + "paint.html";
 		else
 			displayError();
